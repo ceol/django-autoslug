a83534bda39031a7ca1f2faf62bf9ec03f48e735
Patrick Jacobs <ceolwulf@gmail.com>
2013-07-25 18:36:31 -0400

    updated setup.py with license and clarity

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
 
 setup(name='django-autoslug',
       version='0.1',
-      description='Automatic slugification of Django models.',
-      long_description=open('README.md').read(),
+      description='Automatic slugification on Django models.',
+      long_description=readme,
       author='Patrick Jacobs',
       url='https://github.com/ceol/django-autoslug',
-      packages=['autoslug'],
+      license=license,
+      packages=find_packages(),
      )
